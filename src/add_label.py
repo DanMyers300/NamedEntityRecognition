332504f29f45233a632aa7b20bd93cc77d41239d
DanMyers300 <contact+github@danmyers.net>
2023-06-23 13:16:34 -0500

    experimenting with pipelines

--- src/add_label.py
+++ src/add_label.py
@@ -3,9 +3,11 @@ import spacy
 MODEL_PATH = "data/custom-models/rfq_model"
 
 nlp = spacy.blank("en")
-ner = nlp.add_pipe("ner", name='rfq_ner')  # Use `add_pipe` directly
+ner = nlp.add_pipe("ner", name='rfq_ner')
 
 ner.add_label("RFQ")
 
-nlp.to_disk(MODEL_PATH)
+print(nlp.pipe_names)
+
+# nlp.to_disk(MODEL_PATH)
 
--- src/train.py
+++
@@ -1,5 +0,0 @@
-import json
-import random
-import spacy
-
-
