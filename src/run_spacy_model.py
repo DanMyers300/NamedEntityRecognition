271c8a032e3a7d5eca15b063a3df7d8b8900c211
DanMyers300 <contact+github@danmyers.net>
2023-06-23 11:11:56 -0500

    another failed attempt

--- src/run_spacy_model.py
+++ src/run_spacy_model.py
@@ -3,14 +3,14 @@ Use the model that was previously created
 """
 import spacy
 
-nlp = spacy.load("src/spacy/outputs/model-best")
+nlp = spacy.load("data/custom-models/model-best")
 
 def open_file(input_file):
     "Open a text file"
     with open(input_file, "r", encoding="utf-8") as opened_file:
         open_text = opened_file.read()
     return open_text
-text = open_file("data/outputs/rfq_dump.txt")
+text = open_file("data/rfq_dump.txt")
 
 doc = nlp(text)
 for ent in doc.ents:
